56a175d78f3bb115fdacde27b47b3691200b1d19
Alston <alston1399@gmail.com>
2024-06-28 16:05:09 -0700

    [Refactor][Debug][Implement]
    Moved projection of triangles into the Vec3 class
    
    Added a temporary light direction Vec3
    Added some temporary gray scale lighting
    
    Changed from stroke() to fill()

--- src/index.ts
+++ src/index.ts
@@ -8,35 +8,8 @@ const projectionMatrix: Matrix = graphics.createProjectionMatrix(90, 1000, 0.1)
 
 let theta: number = 0
 
-function projectTriangle(triangle: Triangle, matrix: Matrix): Triangle {
-  const p1Mat: Matrix = Matrix.FromArr([...triangle.p1, 1])
-  const p2Mat: Matrix = Matrix.FromArr([...triangle.p2, 1])
-  const p3Mat: Matrix = Matrix.FromArr([...triangle.p3, 1])
-
-  const projectedP1Mat: Matrix = p1Mat.dot(matrix)
-  const projectedP2Mat: Matrix = p2Mat.dot(matrix)
-  const projectedP3Mat: Matrix = p3Mat.dot(matrix)
-
-  const projectedP1Arr: number[] = projectedP1Mat.toArray()
-  const projectedP2Arr: number[] = projectedP2Mat.toArray()
-  const projectedP3Arr: number[] = projectedP3Mat.toArray()
-
-  const projectedP1: Vec3 = new Vec3(...projectedP1Arr)
-  const projectedP2: Vec3 = new Vec3(...projectedP2Arr)
-  const projectedP3: Vec3 = new Vec3(...projectedP3Arr)
-
-  const p1W: number = projectedP1Arr[3]
-  const p2W: number = projectedP2Arr[3]
-  const p3W: number = projectedP3Arr[3]
-
-  if (p1W != 0) Vec3.Scale(projectedP1, 1 / p1W)
-  if (p2W != 0) Vec3.Scale(projectedP2, 1 / p2W)
-  if (p3W != 0) Vec3.Scale(projectedP3, 1 / p3W)
-
-  return new Triangle(projectedP1, projectedP2, projectedP3)
-}
-
 const cameraPos: Vec3 = new Vec3()
+const lightDir: Vec3 = new Vec3(0, 0, -1)
 
 let lastTimestamp: number = 0
 function drawLoop(timestamp: number = 0): void {
@@ -45,12 +18,15 @@ function drawLoop(timestamp: number = 0): void {
 
 
 
+
+
+
+
   cube.rotate(deltaTime / 1000, 0, 0)
 
 
 
   graphics.bg()
-  graphics.strokeStyle = '#fff'
 
   const mutatedTriangles: Triangle[] = cube.applyMatrices()
   const filtered: Triangle[] = mutatedTriangles.filter(triangle => {
@@ -58,23 +34,22 @@ function drawLoop(timestamp: number = 0): void {
   })
 
   for (const triangle of filtered) {
-    const projectedTriangle: Triangle = projectTriangle(triangle, projectionMatrix)
+    const normal: Vec3 = triangle.getNormal()
+    const dp: number = lightDir.dot(normal)
+
+    const s: string = Math.round(lerp(dp, 0, 1, 10, 250)).toString(16)
+    graphics.fillStyle = '#' + s + s + s
+
+    const projectedTriangle: Triangle = triangle.project(projectionMatrix)
+
+
 
     graphics.triangleToScreenSpace(projectedTriangle)
   }
 
-  // const outputVecArr: number[] = mutated.toArray()
-  // const outputVec: Vec3 = new Vec3(...outputVecArr)
 
-  // const w: number = outputVecArr[3]
 
-  // if (w != 0) {
-  //   outputVec.x /= w
-  //   outputVec.y /= w
-  //   outputVec.z /= w
-  // }
 
-  // return outputVec
 
 
   window.requestAnimationFrame(drawLoop)
