31330e6272a6422f32adb53d3dbdaaaff182c0e9
Alston <alston1399@gmail.com>
2024-07-06 01:39:09 -0500

    [Cleanup] Graphics
    triangle()
      - Removed commented code
      - Added stroke()
    
    triangleToScreenSpace()
      - Now returns a scaled triangle for the screen space, no longer
      drawing it right away

--- src/Graphics.ts
+++ src/Graphics.ts
@@ -94,24 +94,12 @@ class Graphics {
   }
 
   triangle(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number): void {
-    // triangle inflation, this is to try to reduce the gaps between touching
-    // triangles, doesn't really look that great since corners get pointy
-
-    // const xc: number = (x1 + x2 + x3) / 3
-    // const yc: number = (y1 + y2 + y3) / 3
-
-    // x1 = xc + (x1 - xc) * 1.02
-    // x2 = xc + (x2 - xc) * 1.02
-    // x3 = xc + (x3 - xc) * 1.02
-    // y1 = yc + (y1 - yc) * 1.02
-    // y2 = yc + (y2 - yc) * 1.02
-    // y3 = yc + (y3 - yc) * 1.02
-
     this.beginPath()
       .moveTo(x1, y1)
       .lineTo(x2, y2)
       .lineTo(x3, y3)
       .closePath()
+      .stroke()
       .fill()
   }
 
@@ -119,17 +107,26 @@ class Graphics {
     this.triangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y)
   }
 
-  triangleToScreenSpace(triangle: Triangle): void {
+  triangleToScreenSpace(triangle: Triangle): Triangle {
     const width: number = this.width
     const height: number = this.height
 
-    this.triangle(
-      (triangle.p1.x + 1) * 0.5 * width,
-      height - (triangle.p1.y + 1) * 0.5 * height,
-      (triangle.p2.x + 1) * 0.5 * width,
-      height - (triangle.p2.y + 1) * 0.5 * height,
-      (triangle.p3.x + 1) * 0.5 * width,
-      height - (triangle.p3.y + 1) * 0.5 * height
+    return new Triangle(
+      new Vec3(
+        (triangle.p1.x + 1) * 0.5 * width,
+        height - (triangle.p1.y + 1) * 0.5 * height,
+        triangle.p1.z
+      ),
+      new Vec3(
+        (triangle.p2.x + 1) * 0.5 * width,
+        height - (triangle.p2.y + 1) * 0.5 * height,
+        triangle.p2.z
+      ),
+      new Vec3(
+        (triangle.p3.x + 1) * 0.5 * width,
+        height - (triangle.p3.y + 1) * 0.5 * height,
+        triangle.p3.z
+      )
     )
   }
 
