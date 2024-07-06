2d3cc00e375bbe34ded7f8512e9543f5acdcd5ae
Alston <alston1399@gmail.com>
2024-07-06 01:40:43 -0500

    [Implement] Triangle
    constructor()
      - The internal points are now copies of their parameters
    
    tuple()
      - Returns a tuple of its points
    
    iterator
      - Returns an iterator of its points
    
    applyMatrix()
      - Applys the specified matrix to all points, modifying them
      - Returns a reference to the triangle

--- src/Triangle.ts
+++ src/Triangle.ts
@@ -4,15 +4,37 @@ class Triangle {
   p3: Vec3
 
   constructor(p1: Vec3, p2: Vec3, p3: Vec3) {
-    this.p1 = p1
-    this.p2 = p2
-    this.p3 = p3
+    this.p1 = p1.copy()
+    this.p2 = p2.copy()
+    this.p3 = p3.copy()
+  }
+
+  tuple(): [Vec3, Vec3, Vec3] {
+    return [this.p1, this.p2, this.p3]
+  }
+
+  *[Symbol.iterator]() {
+    yield this.p1
+    yield this.p2
+    yield this.p3
   }
 
   getNormal(): Vec3 {
     const L1: Vec3 = this.p2.sub(this.p1)
     const L2: Vec3 = this.p3.sub(this.p1)
     return L1.cross(L2).normal()
+  }
+
+  applyMatrix(matrix: Matrix): Triangle {
+    for (const point of [this.p1, this.p2, this.p3]) {
+      const mat: Matrix = Matrix.FromArr([...point, 1])
+      const modifiedMat: Matrix = mat.dot(matrix)
+      const modifiedArr: number[] = modifiedMat.toArray()
+      point.x = modifiedArr[0]
+      point.y = modifiedArr[1]
+      point.z = modifiedArr[2]
+    }
+    return this
   }
 
   project(matrix: Matrix): Triangle {
