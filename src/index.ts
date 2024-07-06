c6ebb0afb66581b72a622ae32b3c836b79d093eb
Alston <alston1399@gmail.com>
2024-07-06 01:43:21 -0500

    [Cleanup][Implement][Debug] index
    Constants
      - Added nearPlane and farPlane
    
    Cleanup
      - Removed commented out initial rotation for the cube
    
    Keybinds
      - Added a keyboard input handler and some keybinds for camera movement
    
    Debug
      - Added logTriangle()
      - Added text()
    
    Utility
      - Added getColor() from the dot product of a triangle's normal and the
      light direction
    
    clipVecAgainstPlane()
      - Clips a Vec3 against the specified plane
    
    clipTriangleAgainstPlane()
      - Clips a Triangle against the specified plane, returning an array of
      the resulting triangles
    
    draw loop
      - Added a raster that will be properly implemented later
      - Added clipping to the near and far planes as well as the canvas
      borders
      - Condensed the transformation matrices (rotation, translation) into
      the worldMatrix
      - Condensed the lookAt matrix and inverse into the viewMatrix

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
+const nearPlane: number = 0.1
+const farPlane: number = 1000
+
 const cube: Cube = new Cube()
   .translate(new Vec3(2, 0, 3))
   .centerPoints()
-// .rotate(0, 0.2, 0.5)
 
 const graphics: Graphics = new Graphics().appendTo(document.body)
 
-const projectionMatrix: Matrix = graphics.createProjectionMatrix(90, 1000, 0.1)
+const projectionMatrix: Matrix = graphics.createProjectionMatrix(90, farPlane, nearPlane)
+
+const keys: Record<string, boolean> = {}
+
+window.addEventListener('keydown', e => keys[e.key] = true)
+window.addEventListener('keyup', e => keys[e.key] = false)
 
 let theta: number = 0
 
@@ -13,38 +20,37 @@ const cameraPos: Vec3 = new Vec3()
 const cameraDir: Vec3 = new Vec3(0, 0, 1)
 const cameraUp: Vec3 = new Vec3(0, 1, 0)
 
-const lightDir: Vec3 = new Vec3(-0.2, 0, -1).normal()
+const lightDir: Vec3 = new Vec3(0.2, 0, -1).normal()
 
-window.addEventListener('keydown', e => {
-  switch (e.key) {
-    case 'ArrowLeft':
-      const left: Vec3 = cameraDir.cross(cameraUp)
-      Vec3.Add(cameraPos, left)
-      break
-    case 'ArrowRight':
-      const right: Vec3 = cameraUp.cross(cameraDir)
-      Vec3.Add(cameraPos, right)
-      break
-    case 'ArrowUp':
-      Vec3.Add(cameraPos, new Vec3(0, 1, 0))
-      break
-    case 'ArrowDown':
-      Vec3.Add(cameraPos, new Vec3(0, -1, 0))
-      break
-    case 'e':
-      Vec3.Add(cameraPos, cameraDir)
-      break
-    case 'q':
-      Vec3.Sub(cameraPos, cameraDir)
-      break
-    case 'd':
-      Vec3.RotateAround(cameraDir, cameraUp, -0.2)
-      break
-    case 'a':
-      Vec3.RotateAround(cameraDir, cameraUp, 0.2)
-      break
-  }
-})
+const keybinds: Record<string, string> = {
+  'Forward': 'w',
+  'Backward': 's',
+  'Left': 'a',
+  'Right': 'd',
+  'Up': 'e',
+  'Down': 'q',
+  'Yaw left': 'ArrowLeft',
+  'Yaw right': 'ArrowRight'
+}
+
+function loadInputs(): void {
+  const FB: number = (keys[keybinds['Forward']] ? 1 : 0) - (keys[keybinds['Backward']] ? 1 : 0)
+  const LR: number = (keys[keybinds['Right']] ? 1 : 0) - (keys[keybinds['Left']] ? 1 : 0)
+  const UD: number = (keys[keybinds['Up']] ? 1 : 0) - (keys[keybinds['Down']] ? 1 : 0)
+  const Y: number = (keys[keybinds['Yaw left']] ? 1 : 0) - (keys[keybinds['Yaw right']] ? 1 : 0)
+
+  Vec3.Add(cameraPos, cameraDir.normal().scale(FB * 0.02))
+  Vec3.Add(cameraPos, cameraUp.cross(cameraDir).normal().scale(LR * 0.02))
+  Vec3.Add(cameraPos, cameraUp.normal().scale(UD * 0.02))
+  Vec3.RotateAround(cameraDir, cameraUp, Y * 0.01)
+}
+
+function logTriangle(tri: Triangle, t?: string): void {
+  if (t) console.log(t)
+  console.log('p1: [' + [...tri.p1].join(', ') + ']')
+  console.log('p2: [' + [...tri.p2].join(', ') + ']')
+  console.log('p3: [' + [...tri.p3].join(', ') + ']')
+}
 
 graphics.context.font = 'arial 10px'
 graphics.context.textAlign = 'left'
@@ -55,62 +61,195 @@ function text(t: string, x: number, y: number): void {
   graphics.context.fillText(t, x, y)
 }
 
+function getLightColor(dp: number): string {
+  const s: string = clamp(Math.round(dp * 255), 30, 250).toString(16)
+  const sMod: string = s.length == 1 ? '0' + s : s
+  return '#' + sMod + sMod + sMod
+}
+
+function clipVecAgainstPlane(planePoint: Vec3, planeNormal: Vec3, lineStart: Vec3, lineEnd: Vec3): Vec3 {
+  Vec3.Normal(planeNormal)
+  const planeD: number = -planeNormal.dot(planePoint)
+  const ad: number = lineStart.dot(planeNormal)
+  const bd: number = lineEnd.dot(planeNormal)
+  const t: number = (-planeD - ad) / (bd - ad)
+  const lineStartToEnd: Vec3 = lineEnd.sub(lineStart)
+  const lineToIntersect: Vec3 = lineStartToEnd.scale(t)
+  return lineStart.add(lineToIntersect)
+}
+
+function clipTriangleAgainstPlane(planePoint: Vec3, planeNormal: Vec3, triangle: Triangle): Triangle[] {
+  Vec3.Normal(planeNormal)
+
+  const dist = (p: Vec3) => planeNormal.dot(p) - planeNormal.dot(planePoint)
+
+  const insidePoints: Vec3[] = []
+  const outsidePoints: Vec3[] = []
+
+  const d0: number = dist(triangle.p1)
+  const d1: number = dist(triangle.p2)
+  const d2: number = dist(triangle.p3)
+
+  if (d0 >= 0) insidePoints[insidePoints.length] = triangle.p1
+  else outsidePoints[outsidePoints.length] = triangle.p1
+  if (d1 >= 0) insidePoints[insidePoints.length] = triangle.p2
+  else outsidePoints[outsidePoints.length] = triangle.p2
+  if (d2 >= 0) insidePoints[insidePoints.length] = triangle.p3
+  else outsidePoints[outsidePoints.length] = triangle.p3
+
+  if (insidePoints.length == 0) return []
+
+  else if (insidePoints.length == 3) return [triangle]
+
+  else if (insidePoints.length == 1) {
+    const p1: Vec3 = insidePoints[0]
+    const p2: Vec3 = clipVecAgainstPlane(planePoint, planeNormal, insidePoints[0], outsidePoints[0])
+    const p3: Vec3 = clipVecAgainstPlane(planePoint, planeNormal, insidePoints[0], outsidePoints[1])
+    return [new Triangle(p1, p2, p3)]
+  }
+
+  else {
+    const p1: Vec3 = insidePoints[0]
+    const p2: Vec3 = insidePoints[1]
+    const p3: Vec3 = clipVecAgainstPlane(planePoint, planeNormal, insidePoints[0], outsidePoints[0])
+    const p4: Vec3 = clipVecAgainstPlane(planePoint, planeNormal, insidePoints[1], outsidePoints[0])
+    return [new Triangle(p1, p2, p3), new Triangle(p2, p3, p4)]
+  }
+}
+
 let lastTimestamp: number = 0
 function drawLoop(timestamp: number = 0): void {
   const deltaTime: number = timestamp - lastTimestamp
   lastTimestamp = timestamp
 
+  theta += deltaTime / 1000
+  theta %= 2 * Math.PI
 
-
-
-
-  cube.rotate(new Vec3(1, 0, 0), deltaTime / 1000)
-
-  const lookAt: Matrix = createLookAtMatrix(cameraPos, cameraPos.add(cameraDir), cameraUp)
-
-  // cube.rotate(deltaTime / 1000, 0, 0)
-
-
+  loadInputs()
 
   graphics.bg()
 
-  // graphics.context.putImageData(imageData, 0, 0)
 
-  const mutatedMesh: Mesh = cube.applyMatrices({ camera: invertLookAtMatrix(lookAt) })
 
-  const mutatedTriangles: MeshTriangle[] = mutatedMesh.triangles
-  const filtered: MeshTriangle[] = mutatedTriangles.filter(triangle => {
-    return triangle.getNormal().dot(triangle.getP1().add(cube.position).sub(cameraPos)) < 0
-  })
-  // const filtered: MeshTriangle[] = mutatedTriangles
 
-  let i = 5
 
-  for (const triangle of filtered) {
-    const normal: Vec3 = triangle.getNormal()
-    const dp: number = lightDir.dot(normal);
+  const rotationMatrix: Matrix = createRotMatQuaternion(new Vec3(1, 0, 0), theta)
+  // const rotationMatrix: Matrix = Matrix.MakeIdentity()
 
-    text('[' + [...normal].join(', ') + ']', 5, i)
-    i += 10
+  const translationMatrix: Matrix = createTranslationMat(new Vec3(0, 0, 3))
 
-    // console.log(normal)
+  const worldMatrix: Matrix = Matrix.MakeIdentity()
+    .dot(rotationMatrix)
+    .dot(translationMatrix)
 
-    const s: string = Math.max(0, Math.min(255, Math.round(dp * 255))).toString(16)
-    const sMod: string = s.length == 1 ? '0' + s : s
-    graphics.fillStyle = '#' + sMod + sMod + sMod
+  const viewMatrix: Matrix = invertLookAtMatrix(createLookAtMatrix(cameraPos, cameraPos.add(cameraDir), cameraUp))
 
-    // let ensure = (s: string) => (s.length == 1 ? '0' + s : s)
 
-    // graphics.fillStyle = '#' +
-    //   ensure(Math.round(lerp(normal.x, -1, 1, 0, 255)).toString(16)) +
-    //   ensure(Math.round(lerp(normal.y, -1, 1, 0, 255)).toString(16)) +
-    //   ensure(Math.round(lerp(normal.z, -1, 1, 0, 255)).toString(16))
 
-    // console.log(graphics.fillStyle)
 
-    const projectedTriangle: Triangle = triangle.project(projectionMatrix)
 
-    graphics.triangleToScreenSpace(projectedTriangle)
+
+  graphics.strokeStyle = '#fff'
+
+  const raster: { triangle: Triangle, color: string }[] = []
+
+  for (const tri of cube.mesh.triangles) {
+    const triangle = new Triangle(tri.getP1(), tri.getP2(), tri.getP3())
+
+    const transformedTriangle: Triangle = triangle.applyMatrix(worldMatrix)
+
+    const normal: Vec3 = transformedTriangle.getNormal()
+
+    const cameraRay: Vec3 = transformedTriangle.p1.sub(cameraPos)
+
+    if (normal.dot(cameraRay) < 0) { // triangle is visible
+      const dp: number = lightDir.dot(normal)
+      const color: string = getLightColor(dp)
+
+      // view
+      const viewedTriangle: Triangle = transformedTriangle.applyMatrix(viewMatrix)
+
+      // clip against near and far planes
+      const clippedTriangles: Triangle[] = []
+      clippedTriangles.push(...clipTriangleAgainstPlane(
+        new Vec3(0, 0, nearPlane),
+        new Vec3(0, 0, 1),
+        viewedTriangle
+      ))
+      const clippedN: number = clippedTriangles.length
+      for (let i = 0; i < clippedN; i++) {
+        const clipped: Triangle = clippedTriangles.shift() as Triangle
+        clippedTriangles.push(...clipTriangleAgainstPlane(
+          new Vec3(0, 0, farPlane),
+          new Vec3(0, 0, -1),
+          clipped
+        ))
+      }
+
+
+      for (const clipped of clippedTriangles) {
+        // project
+        const projected: Triangle = clipped.project(projectionMatrix)
+
+        // scale
+        const scaled: Triangle = graphics.triangleToScreenSpace(projected)
+
+        raster.push({ triangle: scaled, color })
+      }
+    }
+  }
+
+  raster.sort((a, b) => b.triangle.p1.z - a.triangle.p1.z)
+
+  for (const obj of raster) {
+    const toClip: Triangle = obj.triangle
+    graphics.fillStyle = obj.color
+    graphics.strokeStyle = obj.color
+
+    const arr: Triangle[] = [toClip]
+
+    for (let i = 0; i < 4; i++) {
+      const toClipN: number = arr.length
+
+      for (let j = 0; j < toClipN; j++) {
+        const clipping: Triangle = arr.shift() as Triangle
+
+        switch (i) {
+          case 0:
+            arr.push(...clipTriangleAgainstPlane(
+              new Vec3(0, 0, 0),
+              new Vec3(1, 0, 0),
+              clipping
+            ))
+            break
+          case 1:
+            arr.push(...clipTriangleAgainstPlane(
+              new Vec3(0, 0, 0),
+              new Vec3(0, 1, 0),
+              clipping
+            ))
+            break
+          case 2:
+            arr.push(...clipTriangleAgainstPlane(
+              new Vec3(graphics.width, 0, 0),
+              new Vec3(-1, 0, 0),
+              clipping
+            ))
+            break
+          case 3:
+            arr.push(...clipTriangleAgainstPlane(
+              new Vec3(0, graphics.height, 0),
+              new Vec3(0, -1, 0),
+              clipping
+            ))
+            break
+        }
+      }
+    }
+
+    for (const clipped of arr) {
+      graphics.triangleFromVec3(...clipped.tuple())
+    }
   }
 
   window.requestAnimationFrame(drawLoop)
