2a26e3c264e88116ca19f62b596991572a267a55
Alston <alston1399@gmail.com>
2024-07-07 15:52:33 -0500

    [Implement][Refactor] Camera
    Created Camera class that extends the MutableObject class

---
+++ src/Camera.ts
@@ -0,0 +1,22 @@
+interface Camera {
+  translate(translation: Vec3): Camera
+  moveTo(position: Vec3): Camera
+  rotate(axis: Vec3, theta: number): Camera
+}
+
+class Camera extends MutableObject {
+  lookAt(target: Vec3, roll?: number): Camera {
+    this.orientation = Matrix.MakeIdentity()
+
+    const diff: Vec3 = target.sub(this.position)
+    const yaw: number = Math.atan2(diff.z, diff.x)
+    const pitch: number = Math.atan2(diff.y, diff.z)
+
+    this.rotate(Vec3.jHat, yaw)
+      .rotate(this.getRight(), pitch)
+
+    if (roll) this.rotate(this.getFront(), roll)
+
+    return this
+  }
+}
