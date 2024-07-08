6cc0e92d81aecd5846130a3634291528ed10403d
Alston <alston1399@gmail.com>
2024-07-08 07:17:22 -0500

    [Implement] Icosphere
    Fully implemented constructor for Icosphere

--- src/shapes/Icosphere.ts
+++ src/shapes/Icosphere.ts
@@ -1,57 +1,64 @@
-class IcoSphere extends MutableShape {
+class Icosphere extends MutableShape {
+  constructor(radius: number = 1, subdivisions: number = 0) {
+    subdivisions = Math.max(subdivisions, 0)
 
+    // points
+    const points: Vec3[] = [
+      new Vec3(-1, GoldenRatio, 0),
+      new Vec3(1, GoldenRatio, 0),
+      new Vec3(-1, -GoldenRatio, 0),
+      new Vec3(1, -GoldenRatio, 0),
+      new Vec3(0, -1, GoldenRatio),
+      new Vec3(0, 1, GoldenRatio),
+      new Vec3(0, -1, -GoldenRatio),
+      new Vec3(0, 1, -GoldenRatio),
+      new Vec3(GoldenRatio, 0, -1),
+      new Vec3(GoldenRatio, 0, 1),
+      new Vec3(-GoldenRatio, 0, -1),
+      new Vec3(-GoldenRatio, 0, 1)
+    ].map(point => point.normal())
+
+    // triangles
+    const triangles: [number, number, number][] = [
+      [0, 11, 5],
+      [0, 5, 1],
+      [0, 1, 7],
+      [0, 7, 10],
+      [0, 10, 11],
+      [1, 5, 9],
+      [5, 11, 4],
+      [11, 10, 2],
+      [10, 7, 6],
+      [7, 1, 8],
+      [3, 9, 4],
+      [3, 4, 2],
+      [3, 2, 6],
+      [3, 6, 8],
+      [3, 8, 9],
+      [4, 9, 5],
+      [2, 4, 11],
+      [6, 2, 10],
+      [8, 6, 7],
+      [9, 8, 1]
+    ]
+
+    super(points, triangles)
+
+    if (subdivisions > 0) {
+      for (let i = 0; i < subdivisions; i++) {
+        const triangles: MeshTriangle[] = [...this.mesh.triangles]
+        for (const triangle of triangles) {
+          MeshTriangle.Subdivide(triangle)
+        }
+        this.mesh.points.forEach(point => Vec3.Normal(point))
+      }
+    }
+
+    this.stretch(radius, radius, radius)
+  }
 }
 
 
-
-
-
-
-// class Icosphere extends SpatialMeshable {
-//   constructor(pos: Vector = new Vector(), subdivisions: number = 0, radius: number = 1, orientation: OrientationMatrix = new OrientationMatrix()) {
-//     let t: number = (1+Math.sqrt(5))/2
-//     let vertecies: Vector[] = [
-//       new Vector(-1,  t,  0),
-//       new Vector( 1,  t,  0),
-//       new Vector(-1, -t,  0),
-//       new Vector( 1, -t,  0),
-      
-//       new Vector( 0, -1,  t),
-//       new Vector( 0,  1,  t),
-//       new Vector( 0, -1, -t),
-//       new Vector( 0,  1, -t),
-      
-//       new Vector( t,  0, -1),
-//       new Vector( t,  0,  1),
-//       new Vector(-t,  0, -1),
-//       new Vector(-t,  0,  1)
-//     ].map((v: Vector) => Vector.Hat(v))
-
-//     let tris: Triangle[] = [
-//       Triangle.FromArr(0, 11, 5, vertecies),
-//       Triangle.FromArr(0, 5, 1, vertecies),
-//       Triangle.FromArr(0, 1, 7, vertecies),
-//       Triangle.FromArr(0, 7, 10, vertecies),
-//       Triangle.FromArr(0, 10, 11, vertecies),
-
-//       Triangle.FromArr(1, 5, 9, vertecies),
-//       Triangle.FromArr(5, 11, 4, vertecies),
-//       Triangle.FromArr(11, 10, 2, vertecies),
-//       Triangle.FromArr(10, 7, 6, vertecies),
-//       Triangle.FromArr(7, 1, 8, vertecies),
-
-//       Triangle.FromArr(3, 9, 4, vertecies),
-//       Triangle.FromArr(3, 4, 2, vertecies),
-//       Triangle.FromArr(3, 2, 6, vertecies),
-//       Triangle.FromArr(3, 6, 8, vertecies),
-//       Triangle.FromArr(3, 8, 9, vertecies),
-      
-//       Triangle.FromArr(4, 9, 5, vertecies),
-//       Triangle.FromArr(2, 4, 11, vertecies),
-//       Triangle.FromArr(6, 2, 10, vertecies),
-//       Triangle.FromArr(8, 6, 7, vertecies),
-//       Triangle.FromArr(9, 8, 1, vertecies)
-//     ]
 
 //     for (let i=0; i<subdivisions; i++) {
 //       let tris2: Triangle[] = []
@@ -66,15 +73,3 @@ class IcoSphere extends MutableShape {
 //       }
 //       tris = tris2
 //     }
-
-//     if (radius != 1) {
-//       for (let tri of tris) {
-//         tri.p1 = Vector.Scale(Vector.Hat(tri.p1), radius)
-//         tri.p2 = Vector.Scale(Vector.Hat(tri.p2), radius)
-//         tri.p3 = Vector.Scale(Vector.Hat(tri.p3), radius)
-//       }
-//     }
-
-//     super(pos, orientation, new Mesh(tris.map((t: Triangle) => t.copy())))
-//   }
-// }
