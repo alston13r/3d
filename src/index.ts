a4aac40c7ef3941fe30f153af7d03f9804a95374
Alston <alston1399@gmail.com>
2024-07-08 06:19:47 -0500

    [Implement][Demo] index
    Added some code to create a UVSphere for implementation and demo
    purposes as well as rudimentary way for rendering multiple shapes

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,17 @@ const cube: Cube = new Cube()
   .translate(new Vec3(2, 0, 3))
   .centerPoints()
 
+const sphere: UVSphere = new UVSphere(0.5, 10, 10)
+  .translate(new Vec3(-2, 0, 3))
+
+const objects: MutableObject[] = [cube, sphere]
+
 const keys: Record<string, boolean> = {}
 
 window.addEventListener('keydown', e => keys[e.key] = true)
 window.addEventListener('keyup', e => keys[e.key] = false)
 
-let theta: number = 0
-
-const lightDir: Vec3 = new Vec3(0.2, 0, -1).normal()
+const lightDir: Vec3 = new Vec3(-0.1, -0.1, 1).normal().scale(-1)
 
 const keybinds: Record<string, string> = {
   'Forward': 'w',
@@ -57,29 +60,58 @@ function drawLoop(timestamp: number = 0): void {
   const deltaTime: number = timestamp - lastTimestamp
   lastTimestamp = timestamp
 
-  theta += deltaTime / 1000
-  theta %= 2 * Math.PI
-
   loadInputs()
 
   scene.graphics.bg()
 
-  const rotationMatrix: Matrix = createRotMatQuaternion(new Vec3(1, 0, 0), theta)
+  cube.rotate(new Vec3(1, 0, 0), 0.008)
 
-  const translationMatrix: Matrix = createTranslationMat(new Vec3(0, 0, 3))
+  const cubeWorldMatrix: Matrix = cube.getWorldMatrix()
+  const sphereWorldMatrix: Matrix = sphere.getWorldMatrix()
 
-  const worldMatrix: Matrix = Matrix.MakeIdentity()
-    .dot(rotationMatrix)
-    .dot(translationMatrix)
-
-  const viewMatrix: Matrix = invertLookAtMatrix(camera.createLookAtMatrix())
+  const viewMatrix: Matrix = camera.createViewMatrix()
 
   const raster: { triangle: Triangle, color: string }[] = []
+
+  // const trianglesToProject: MeshTriangle[] = cube.mesh.triangles.concat(sphere.mesh.triangles)
 
   for (const tri of cube.mesh.triangles) {
     const triangle = new Triangle(tri.getP1(), tri.getP2(), tri.getP3())
 
-    const transformedTriangle: Triangle = triangle.applyMatrix(worldMatrix)
+    const transformedTriangle: Triangle = triangle.applyMatrix(cubeWorldMatrix)
+
+    const cameraRay: Vec3 = transformedTriangle.p1.sub(camera.position)
+
+    if (cameraRay.dot(camera.getFront()) < 0) continue // triangle is behind camera
+
+    const normal: Vec3 = transformedTriangle.getNormal()
+
+    if (normal.dot(cameraRay) < 0) { // triangle face is visible
+      const dp: number = lightDir.dot(normal)
+      const color: string = Color.FromGrey(clamp(Math.round(dp * 255), 30, 250)).toString()
+
+      // view
+      const viewedTriangle: Triangle = transformedTriangle.applyMatrix(viewMatrix)
+
+      // clip against near and far planes
+      const triangles: Triangle[] = scene.clipTriangleAgainstNearFarPlanes(viewedTriangle)
+
+      for (const clipped of triangles) {
+        // project
+        const projected: Triangle = clipped.project(scene.projectionMatrix)
+
+        // scale
+        const scaled: Triangle = scene.graphics.triangleToScreenSpace(projected)
+
+        raster.push({ triangle: scaled, color })
+      }
+    }
+  }
+
+  for (const tri of sphere.mesh.triangles) {
+    const triangle = new Triangle(tri.getP1(), tri.getP2(), tri.getP3())
+
+    const transformedTriangle: Triangle = triangle.applyMatrix(sphereWorldMatrix)
 
     const cameraRay: Vec3 = transformedTriangle.p1.sub(camera.position)
 
