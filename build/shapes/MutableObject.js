214e95070bee64749fc1a6ca8ef0fc0a6d9fb8a2
Alston <alston1399@gmail.com>
2024-07-11 20:39:05 -0700

    [Build]

--- build/shapes/MutableObject.js
+++ build/shapes/MutableObject.js
@@ -1,30 +1,30 @@
 "use strict";
-// class MutableObject {
-//   position: Vec3 = vec3.create()
-//   orientation: Mat4 = mat4.create()
-//   translate(translation: Vec3): MutableObject {
-//     vec3.add(this.position, this.position, translation)
-//     return this
-//   }
-//   moveTo(position: Vec3): MutableObject {
-//     Vec3.CopyFrom(this.position, position)
-//     return this
-//   }
-//   rotate(axis: Vec3, theta: number): MutableObject {
-//     this.orientation = Matrix.Dot(this.orientation, createRotMatQuaternion(axis, theta))
-//     return this
-//   }
-//   getRight(): Vec3 {
-//     return new Vec3(...this.orientation.mat[0])
-//   }
-//   getUp(): Vec3 {
-//     return new Vec3(...this.orientation.mat[1])
-//   }
-//   getFront(): Vec3 {
-//     return new Vec3(...this.orientation.mat[2])
-//   }
-//   getWorldMatrix(): Matrix {
-//     return this.orientation.dot(createTranslationMat(this.position))
-//   }
-// }
+class MutableObject {
+    position = vec3.create();
+    orientation = mat4.create();
+    translate(translation) {
+        vec3.add(this.position, this.position, translation);
+        return this;
+    }
+    moveTo(position) {
+        vec3.copy(this.position, position);
+        return this;
+    }
+    rotate(axis, theta) {
+        mat4.rotate(this.orientation, this.orientation, theta, axis);
+        return this;
+    }
+    getRight() {
+        return vec3.fromValues(this.orientation[0], this.orientation[1], this.orientation[2]);
+    }
+    getUp() {
+        return vec3.fromValues(this.orientation[4], this.orientation[5], this.orientation[6]);
+    }
+    getFront() {
+        return vec3.fromValues(this.orientation[8], this.orientation[9], this.orientation[10]);
+    }
+    getWorldMatrix() {
+        return mat4.multiply(mat4.create(), this.orientation, mat4.fromTranslation(mat4.create(), this.position));
+    }
+}
 //# sourceMappingURL=MutableObject.js.map
