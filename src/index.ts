2e2668b896534a002e00c062219ee26148b48459
Alston <alston1399@gmail.com>
2024-07-11 18:43:04 -0700

    [Implement] WebGL
    Imported and converted Mozilla guide code to WebGL

--- src/index.ts
+++ src/index.ts
@@ -1,134 +1,211 @@
-const scene: Scene = new Scene()
-
-const camera: Camera = scene.camera
-
-const cube: Cube = new Cube()
-  .translate(new Vec3(2, 0, 3))
-  .centerPoints()
-
-const uvsphere: UVSphere = new UVSphere(0.5, 10, 10)
-  .translate(new Vec3(-2, 0, 3))
-
-const icosphere: Icosphere = new Icosphere(0.5, 2)
-  .translate(new Vec3(0, 2, 3))
-
-const objects: MutableObject[] = [cube, uvsphere, icosphere]
-
-const keys: Record<string, boolean> = {}
-
-window.addEventListener('keydown', e => keys[e.key] = true)
-window.addEventListener('keyup', e => keys[e.key] = false)
-
-const lightDir: Vec3 = new Vec3(-0.1, -0.1, 1).normal().scale(-1)
-
-const keybinds: Record<string, string> = {
-  'Forward': 'w',
-  'Backward': 's',
-  'Left': 'a',
-  'Right': 'd',
-  'Up': 'e',
-  'Down': 'q',
-  'Yaw left': 'ArrowLeft',
-  'Yaw right': 'ArrowRight',
-  'Pitch up': 'ArrowDown',
-  'Pitch down': 'ArrowUp'
-}
-
-function projectShape(shape: MutableShape, viewMatrix: Matrix, raster: { triangle: Triangle, color: string }[]): void {
-  const worldMatrix: Matrix = shape.getWorldMatrix()
-
-  for (const tri of shape.mesh.triangles) {
-    const triangle = new Triangle(tri.getP1(), tri.getP2(), tri.getP3())
-
-    const transformedTriangle: Triangle = triangle.applyMatrix(worldMatrix)
-
-    const cameraRay: Vec3 = transformedTriangle.p1.sub(camera.position)
-
-    if (cameraRay.dot(camera.getFront()) < 0) continue // triangle is behind camera
-
-    const normal: Vec3 = transformedTriangle.getNormal()
-
-    if (normal.dot(cameraRay) < 0) { // triangle face is visible
-      const dp: number = lightDir.dot(normal)
-      const color: string = Color.FromGrey(clamp(Math.round(dp * 255), 30, 250)).toString()
-
-      // view
-      const viewedTriangle: Triangle = transformedTriangle.applyMatrix(viewMatrix)
-
-      // clip against near and far planes
-      const triangles: Triangle[] = scene.clipTriangleAgainstNearFarPlanes(viewedTriangle)
-
-      for (const clipped of triangles) {
-        // project
-        const projected: Triangle = clipped.project(scene.projectionMatrix)
-
-        // scale
-        const scaled: Triangle = scene.graphics.triangleToScreenSpace(projected)
-
-        raster.push({ triangle: scaled, color })
-      }
-    }
+type ProgramInfo = {
+  program: WebGLProgram,
+  attribLocations: {
+    vertexPosition: GLint
+    textureCoord: GLint
+    vertexColor?: GLint
+    vertexNormal: GLint
+  },
+  uniformLocations: {
+    projectionMatrix: WebGLUniformLocation | null
+    modelViewMatrix: WebGLUniformLocation | null
+    normalMatrix: WebGLUniformLocation | null
+    uSampler: WebGLUniformLocation | null
   }
 }
 
-function loadInputs(): void {
-  const FB: number = (keys[keybinds['Forward']] ? 1 : 0) - (keys[keybinds['Backward']] ? 1 : 0)
-  const LR: number = (keys[keybinds['Right']] ? 1 : 0) - (keys[keybinds['Left']] ? 1 : 0)
-  const UD: number = (keys[keybinds['Up']] ? 1 : 0) - (keys[keybinds['Down']] ? 1 : 0)
-  const Y: number = (keys[keybinds['Yaw left']] ? 1 : 0) - (keys[keybinds['Yaw right']] ? 1 : 0)
-  const P: number = (keys[keybinds['Pitch up']] ? 1 : 0) - (keys[keybinds['Pitch down']] ? 1 : 0)
+let cubeRotation = 0
+let deltaTime = 0
 
-  const right: Vec3 = camera.getRight()
-  const up: Vec3 = camera.getUp()
-  const front: Vec3 = camera.getFront()
+main()
 
-  camera.translate(front.normal().scale(FB * 0.02))
-  camera.translate(right.normal().scale(LR * 0.02))
-  camera.translate(up.normal().scale(UD * 0.02))
+function main(): void {
+  const canvas = document.body.appendChild(document.createElement('canvas'))
+  canvas.width = 800
+  canvas.height = 600
+  const gl = canvas.getContext('webgl') as WebGLRenderingContext
+  if (gl === null) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.")
+    return
+  }
 
-  camera.rotate(up, Y * 0.01)
-  camera.rotate(camera.getRight(), P * 0.01)
-}
+  gl.clearColor(0, 0, 0, 1)
+  gl.clear(gl.COLOR_BUFFER_BIT)
 
-function getLightColor(dp: number): string {
-  const x: number = clamp(Math.round(dp * 255), 30, 250)
-  return new Color(x, x, x).toString()
-}
+  const vsSource = `
+    attribute vec4 aVertexPosition;
+    attribute vec3 aVertexNormal;
+    attribute vec2 aTextureCoord;
 
-let lastTimestamp: number = 0
-function drawLoop(timestamp: number = 0): void {
-  const deltaTime: number = timestamp - lastTimestamp
-  lastTimestamp = timestamp
+    uniform mat4 uNormalMatrix;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
 
-  loadInputs()
+    varying highp vec2 vTextureCoord;
+    varying highp vec3 vLighting;
 
-  scene.graphics.bg()
+    void main(void) {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      vTextureCoord = aTextureCoord;
 
-  cube.rotate(new Vec3(1, 0, 0), 0.008)
+      highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+      highp vec3 directionalLightColor = vec3(1, 1, 1);
+      highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
 
-  const viewMatrix: Matrix = camera.createViewMatrix()
+      highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
 
-  const raster: { triangle: Triangle, color: string }[] = []
+      highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+      vLighting = ambientLight + (directionalLightColor * directional);
+    }
+  `
 
-  projectShape(cube, viewMatrix, raster)
-  projectShape(uvsphere, viewMatrix, raster)
-  projectShape(icosphere, viewMatrix, raster)
+  const fsSource = `
+    varying highp vec2 vTextureCoord;
+    varying highp vec3 vLighting;
 
-  raster.sort((a, b) => b.triangle.p1.z - a.triangle.p1.z)
+    uniform sampler2D uSampler;
 
-  for (const obj of raster) {
-    const toClip: Triangle = obj.triangle
-    scene.graphics.fillStyle = obj.color
-    scene.graphics.strokeStyle = obj.color
+    void main(void) {
+      highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
 
-    const triangles: Triangle[] = scene.clipTriangleAgainstBorderPlanes(toClip)
+      gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
+    }
+  `
 
-    for (const clipped of triangles) {
-      scene.graphics.triangleFromInstance(clipped)
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource) as WebGLProgram
+
+  // Collect all the info needed to use the shader program.
+  // Look up which attributes our shader program is using
+  // for aVertexPosition, aVertexColor and also
+  // look up uniform locations.
+  const programInfo: ProgramInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      vertexNormal: gl.getAttribLocation(shaderProgram, 'aVertexNormal'),
+      textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord')
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+      normalMatrix: gl.getUniformLocation(shaderProgram, 'uNormalMatrix'),
+      uSampler: gl.getUniformLocation(shaderProgram, 'uSampler')
     }
   }
 
-  window.requestAnimationFrame(drawLoop)
+  const buffers = initBuffers(gl)
+
+  // Load texture
+  // const texture = loadTexture(gl, "./cubetexture.png") as WebGLTexture
+  const texture = loadTexture(gl, cubeTextureURI) as WebGLTexture
+  // Flip image pixels into the bottom-to-top order that WebGL expects.
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true)
+
+  let then = 0
+
+  function render(now: number) {
+    now *= 0.001
+    deltaTime = now - then
+    then = now
+
+    drawScene(gl, programInfo, buffers, texture, cubeRotation)
+    cubeRotation += deltaTime
+
+    requestAnimationFrame(render)
+  }
+
+  requestAnimationFrame(render)
 }
 
-window.requestAnimationFrame(drawLoop)
+function loadTexture(gl: WebGLRenderingContext, url: string): WebGLTexture {
+  const texture = gl.createTexture() as WebGLTexture
+  gl.bindTexture(gl.TEXTURE_2D, texture)
+
+  // Because images have to be downloaded over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0
+  const internalFormat = gl.RGBA
+  const width = 1
+  const height = 1
+  const border = 0
+  const srcFormat = gl.RGBA
+  const srcType = gl.UNSIGNED_BYTE
+  const pixel = new Uint8Array([0, 0, 255, 255]) // opaque blue
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    level,
+    internalFormat,
+    width,
+    height,
+    border,
+    srcFormat,
+    srcType,
+    pixel
+  )
+
+  const image = new Image()
+  image.onload = () => {
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      level,
+      internalFormat,
+      srcFormat,
+      srcType,
+      image
+    )
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+      // Yes, it's a power of 2. Generate mips.
+      gl.generateMipmap(gl.TEXTURE_2D)
+    } else {
+      // No, it's not a power of 2. Turn off mips and set
+      // wrapping to clamp to edge
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    }
+  }
+  image.src = url
+
+  return texture
+}
+
+function isPowerOf2(value: number) {
+  return (value & (value - 1)) === 0
+}
+
+function initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string): WebGLProgram | null {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource) as WebGLShader
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource) as WebGLShader
+
+  const shaderProgram = gl.createProgram() as WebGLProgram
+  gl.attachShader(shaderProgram, vertexShader)
+  gl.attachShader(shaderProgram, fragmentShader)
+  gl.linkProgram(shaderProgram)
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert(`Unable to initialize the shader program: ${gl.getProgramInfoLog(shaderProgram)}`)
+    return null
+  }
+
+  return shaderProgram
+}
+
+function loadShader(gl: WebGLRenderingContext, type: GLenum, source: string): WebGLShader | null {
+  const shader = gl.createShader(type) as WebGLShader
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(`An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`)
+    gl.deleteShader(shader)
+    return null
+  }
+  return shader
+}
